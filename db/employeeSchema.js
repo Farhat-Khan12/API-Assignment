e2338ba2a3ac5b3c1b4c3461b7f92cf1440e800f
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-01-27 17:08:30 +0530

    Add files via upload

--- db/employeeSchema.js
+++ db/employeeSchema.js
@@ -1,1 +1,18 @@
+const empSchema = new mongoose.Schema({
+    ename: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    salary: {
+        type: Number,
+        required: true,
 
+    },
+    add: {
+        type: String,
+        required: true,
+
+    }
+})
+module.exports = mongoose.model("employee", empSchema);
