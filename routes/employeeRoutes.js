fb1bde460bc9dabd31b7c4f0c0f7e2a5232ee1e0
Farhat-Khan12 <90671950+Farhat-Khan12@users.noreply.github.com>
2022-01-27 17:07:29 +0530

    Add files via upload

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,1 +1,25 @@
+const express=require('express');
+const router=express.Router();
+const {getData,putData,deleteData}=require('../controller/empController)
+router.get("/",(req,res)=>{
+res.send("get Data")
+getData();
+})
 
+router.post("/addnew",(req,res)=>{
+res.send("post data");
+postData(req.body);
+})
+
+router.put("/update/:id",(req,res)=>{
+const id=req.params.id
+res.send("updated data");
+putData(id,req.body)
+})
+
+router.delete("/delete/:id",(req,res)=>{
+const id=req.params.id;
+res.send("deleted data");
+deleteData(id)
+})
+module.exports=router;
